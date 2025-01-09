a8abe625dedc4dca730792146a57a2b8499938f8
Madness-Incorp <oisinmlynch04@gmail.com>
2025-01-09 23:50:16 +0000

    Small GUI edit

--- setuppage.cpp
+++ setuppage.cpp
@@ -4,41 +4,39 @@
 #include <QPushButton>
 
 setUpPage::setUpPage(QWidget *parent) : QWidget(parent) {
+    setFixedSize(420,300);
     setUp1();
 }
 
 void setUpPage::setUp1() {
     label = new QLabel("Bartmoss Cloud Setup", this);
-    label->setGeometry(100, 100, 200, 40);
+    label->setGeometry(145, 100, 200, 40);
     lineEdit = new QLineEdit(this);
-    lineEdit->setGeometry(100, 150, 200, 30);
+    lineEdit->setGeometry(50, 150, 300, 30);
     lineEdit->hide();
     nextButton = new QPushButton("Next", this);
-    nextButton->setGeometry(100, 150, 100, 30); // Positioning the button
+    nextButton->setGeometry(160, 150, 100, 30); // Positioning the button
     connect(nextButton, &QPushButton::clicked, this, &setUpPage::goToStep2);
 }
 
 void setUpPage::setUp2() {
     label->setText("Enter file Locations");
-    label->setGeometry(100, 50, 200, 40);
+    label->setGeometry(50, 70, 200, 40);
     lineEdit->show();
-    nextButton->setGeometry(100, 250, 100, 30); // Position next button
+    nextButton->setGeometry(50, 220, 100, 30); // Position next button
     connect(nextButton, &QPushButton::clicked, this, &setUpPage::goToStep3);
 }
 
 void setUpPage::setUp3() {
     label->setText("Enter Log Location");
-    label->setGeometry(100, 50, 200, 40);
-    nextButton->setGeometry(100, 250, 100, 30); // Position next button
     connect(nextButton, &QPushButton::clicked, this, &setUpPage::goToStep4);
 }
 
 void setUpPage::setUp4() {
     delete nextButton;
     label->setText("Enter FIFO Location");
-    label->setGeometry(100, 50, 200, 40); // Position label
     finishButton = new QPushButton("Finish", this);
-    finishButton->setGeometry(100, 250, 100, 30);
+    finishButton->setGeometry(50, 220, 100, 30);
     finishButton->show();// Position finish button
     connect(finishButton, &QPushButton::clicked, this, &setUpPage::finishSetup);
 }
